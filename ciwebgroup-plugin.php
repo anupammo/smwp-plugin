8aa2fc7695963454402b09314ab299a65287b7fc
anupammo <a09051985@gmail.com>
2022-12-08 02:38:25 +0530

    081220220238am

--- ciwebgroup-plugin.php
+++ ciwebgroup-plugin.php
@@ -1,3 +1,4 @@
+<?php 
 /**
 * Plugin Name: CIwebGroup-Plugin
 * Plugin URI: https://www.ciwebgroup.com/ciwebgroup-plugin/
@@ -13,3 +14,36 @@
 * Text Domain: ciwebgroup-plugin
 * Domain Path: /languages
 */
+
+
+/**
+ * Register the "book" custom post type
+ */
+function pluginprefix_setup_post_type() {
+	register_post_type( 'book', ['public' => true ] ); 
+} 
+add_action( 'init', 'pluginprefix_setup_post_type' );
+
+
+/**
+ * Activate the plugin.
+ */
+function pluginprefix_activate() { 
+	// Trigger our function that registers the custom post type plugin.
+	pluginprefix_setup_post_type(); 
+	// Clear the permalinks after the post type has been registered.
+	flush_rewrite_rules(); 
+}
+register_activation_hook( __FILE__, 'pluginprefix_activate' );
+
+
+/**
+ * Deactivation hook.
+ */
+function pluginprefix_deactivate() {
+	// Unregister the post type, so the rules are no longer in memory.
+	unregister_post_type( 'book' );
+	// Clear the permalinks to remove our post type's rules from the database.
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'pluginprefix_deactivate' );
