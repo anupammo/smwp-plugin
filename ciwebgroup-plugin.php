1e1ae12b3b23c9591effbdcb47a268f1405dda59
anupammo <a09051985@gmail.com>
2023-01-02 21:53:19 +0530

    020120232153pm

--- ciwebgroup-plugin.php
+++ ciwebgroup-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: CIwebGroup-Plugin
  * Plugin URI: https://www.ciwebgroup.com/ciwebgroup-plugin/
  * Description: Extendify the blocks and template features of Elementor. Also can get lots of new template designes for pages and blocks for Elementor.
- * Version: 1.1.0
+ * Version: 1.2.0
  * Requires at least: 5.2
  * Requires PHP: 7.2
  * Author: Anupam Mondal
@@ -17,73 +17,47 @@
  */
 
 
-function ourPluginSettingsLink()
+class CIWebGroupPlugin
 {
-	add_options_page('CI Web Group Settings', 'CI Web Group', 'manage_options', 'ci-web-group-settings-page', 'ourSettingsPageHTML');
+	function __construct()
+	{
+		add_action('admin_menu', array($this, 'adminPage'));
+		add_action('admin_init', array($this, 'settings'));
+	}
+
+	function settings()
+	{
+		add_settings_section('ciwg_first_section', null, null, 'ci-web-group-settings-page');
+		add_settings_field('ciwg_location', 'Output Position', array($this, 'locationHTML'), 'ci-web-group-settings-page', 'ciwg_first_section');
+		register_setting('ciwebgroupplugin', 'ciwg_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+	}
+
+	function locationHTML()
+	{ ?>
+		<select name="ciwg_location">
+			<option value="0">At starting position</option>
+			<option value="1">At ending position</option>
+		</select>
+	<?php }
+
+	function adminPage()
+	{
+		add_options_page('CI Web Group Settings', 'CI Web Group', 'manage_options', 'ci-web-group-settings-page', array($this, 'settingsHTML'));
+	}
+
+	function settingsHTML()
+	{
+	?>
+		<h1>Settings of CI Web Group</h1>
+		<form action="options.php" method="POST">
+			<?php 
+			settings_fields('ciwebgroupplugin');
+			do_settings_sections('ci-web-group-settings-page');
+			submit_button();
+			?>
+		</form>
+	<?php
+	}
 }
 
-function ourSettingsPageHTML()
-{
-	wp_enqueue_style('style',  plugin_dir_url(__FILE__) . 'public/css/style.css');
-	wp_enqueue_script('script',  plugin_dir_url(__FILE__) . 'public/js/admin-setting.js');
-?>
-
-	<h1>Settings of CI Web Group</h1>
-	<hr />
-	<form class="plugin-form" action="">
-		<p id="demo"></p>
-		<ol class="ol-style">
-			<li>
-				<p class="mb-5">Do you have Facebook account?</p>
-				<input type="radio" id="fbYes" name="fb_ac" value="Yes" onchange="fburlYes()">
-				<label>Yes</label>
-				<input type="radio" id="fbNo" name="fb_ac" value="No" onchange="fburlNo()">
-				<label>No</label>
-				<br />
-				<div id="fburlyes">
-					<label class="my-10">URL of your Facebook account / page</label>
-					<input type="url" id="fb_url" disabled>
-				</div>
-			</li>
-			<li>
-				<p class="mb-5">Do you have Linkedin account?</p>
-				<input type="radio" id="liYes" name="li_ac" value="Yes" onchange="liurlYes()">
-				<label>Yes</label>
-				<input type="radio" id="liNo" name="li_ac" value="No" onchange="liurlNo()">
-				<label>No</label>
-				<br />
-				<div id="liurlyes">
-					<label class="my-10">URL of your Linkedin account / page</label>
-					<input type="url" id="li_url" disabled>
-				</div>
-			</li>
-			<li>
-				<p class="mb-5">Do you have Instagram account?</p>
-				<input type="radio" id="instaYes" name="insta_ac" value="Yes" onchange="instaurlYes()">
-				<label>Yes</label>
-				<input type="radio" id="instaNo" name="insta_ac" value="No" onchange="instaurlNo()">
-				<label>No</label>
-				<br />
-				<div id="instaurlYes">
-					<label class="my-10">URL of your Instagram account / page</label>
-					<input type="url" id="insta_url" disabled>
-				</div>
-			</li>
-			<li>
-				<p class="mb-5">Do you have Twitter account?</p>
-				<input type="radio" id="tweetYes" name="tweet_ac" value="Yes" onchange="tweeturlYes()">
-				<label>Yes</label>
-				<input type="radio" id="tweetNo" name="tweet_ac" value="No" onchange="tweeturlNo()">
-				<label>No</label>
-				<br />
-				<div id="tweeturlyes">
-					<label class="my-10">URL of your Twitter account / page</label>
-					<input type="url" id="tweet_url" disabled>
-				</div>
-			</li>
-		</ol>
-	</form>
-
-<?php }
-
-add_action('admin_menu', 'ourPluginSettingsLink');
+$cIWebGroupPlugin = new CIWebGroupPlugin();
